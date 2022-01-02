bad864e98b5e2ba63d038fe64c424c13d9760cf2
peermohsin22 <peermohsin22@gmail.com>
2022-01-02 13:27:09 +0530

    Merge Sort

--- AlgorithmProbs/Program.cs
+++ AlgorithmProbs/Program.cs
@@ -4,18 +4,11 @@ namespace AlgorithmProbs
 {
     class Program
     {
-        static void Main(string[] args)
+       static void Main(string[] args)
         {
-            int[] arr1 = { 23, 52, 84, 63, 41, 95, 29 };
-            Console.WriteLine("The Array Before Bubble Sort : ");
-            int n = arr1.Length;
-            for (int i = 0; i < n; ++i)
-            {
-                Console.Write(arr1[i] + " ");
-            }
-            BubbleSort.BubbleSortArray(arr1);
-            BubbleSort.PrintArray(arr1);
-
+            Console.WriteLine("Welcome to Algorithm Programs! ");
+            MergeSort mergeSort = new MergeSort();
+            mergeSort.Merging();
         }
     }
 }
