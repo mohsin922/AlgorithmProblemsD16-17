fba5460599005ffe80d0ce620e9dafc7c7ad3c8b
peermohsin22 <peermohsin22@gmail.com>
2022-01-02 13:31:27 +0530

    Strings are Anagrams

--- AlgorithmProbs/Program.cs
+++ AlgorithmProbs/Program.cs
@@ -4,11 +4,11 @@ namespace AlgorithmProbs
 {
     class Program
     {
-       static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Algorithm Programs! ");
-            MergeSort mergeSort = new MergeSort();
-            mergeSort.Merging();
+            Console.WriteLine("Welcome to Algorithm Problems!");
+            Anagram d = new Anagram();
+            d.AnagramDetector();
         }
     }
 }
