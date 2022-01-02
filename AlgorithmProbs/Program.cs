b5bca7a354e7d756118d3122324ee3f9680f3439
peermohsin22 <peermohsin22@gmail.com>
2022-01-02 13:13:20 +0530

    Binary Search Word From Word List

---
+++ AlgorithmProbs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AlgorithmProbs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithm Programs!");
+
+            Console.WriteLine("\nBinary Search the word from wordlist");
+            string txtpath = File.ReadAllText(@"C:\Users\MOHSIN ZAHOOR PEER\Desktop\AlgorithmProbs\AlgorithmProbs\Alg.txt");
+            List<string> words = new List<string>(txtpath.Split());
+            words.Sort();
+            BinarySearch.BinarySearchWord(words);
+        }
+    }
+}
